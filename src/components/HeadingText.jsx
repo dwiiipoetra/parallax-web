fa70bedfc0ff97f3485a62e151c2be727e95543a
dwiiipoetra2 <dwi.waluyoputranto@gmail.com>
2025-08-08 04:58:26 +0700

    add HeadingText component

---
+++ src/components/HeadingText.jsx
@@ -0,0 +1,17 @@
+import { motion } from "motion/react"
+
+const HeadingText = ({ text, mb = 6, color = "text-yellow-400" }) => {
+  return (
+    <motion.h2
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.7 }}
+        viewport={{ once: true }}
+        className={`text-3xl font-bold mb-${mb} ${color} text-center`}
+    >
+    {text}
+    </motion.h2>
+  )
+}
+
+export default HeadingText
